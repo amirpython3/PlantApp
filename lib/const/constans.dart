43542c40f4abeed69e41d644c731b3f3f65aed98
Amir <amirpython3@gmail.com>
2025-09-11 13:13:05 +0330

    add constans color / title

--- lib/const/constans.dart
+++ lib/const/constans.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Constans {
+  static Color primerycolor = Color(0xFF296E48);
+  static Color blackcolor = Colors.black;
+  static String titleOne = "گیاهان را بهتر از قبل درک کن";
+  static String descriptionOne =
+      "در مورد نگه داری گل و گیاهان میتوانی اطلاعات کسب کنی";
+  static String titleTwo = "با گیاهان جدید آشنا شو";
+  static String descriptionTwo =
+      "رز مشکی یا گل رز دوست داری؟ اینجا میتونی پیداش کنی";
+  static String titleThree = "با یک گل بهار نمیشود، گل بکار";
+  static String descriptionThree =
+      "هر گلی نیاز داشته باشید در این اپلیکیشن پیدا میکنید";
+}
